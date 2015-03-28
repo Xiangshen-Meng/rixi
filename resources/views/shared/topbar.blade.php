d306b30ac8d4a8fe9a3d9e981b39587e66b67333
Xiangshen-Meng <wqnnjm@gmail.com>
2015-03-28 17:31:58 +0900

    Added Login

--- resources/views/shared/topbar.blade.php
+++ resources/views/shared/topbar.blade.php
@@ -1,15 +1,15 @@
 <div class="topbar">
   <div class="container">
     <div class="row">
-      <div class="col-xs-7 col-xs-offset-1 col-sm-10 col-sm-offset-0">
-        <h1>日系</h1>
+      <div class="col-xs-3 col-xs-offset-1 col-sm-2 col-sm-offset-0">
+        <a href="{{ url('/') }}"><h1>日系</h1></a>
       </div>
-      <div class="col-xs-3 col-sm-2">
+      <div class="col-xs-3 col-xs-offset-4 col-sm-2 col-sm-offset-8">
         <div class="topbar-login">
           @if (Auth::guest())
             <a href="{{ url('/auth/login') }}" type="button" class="btn btn-primary">我要登录</a>
           @else
-            <a href="{{ url('/auth/logout') }}">{{ Auth::user()->name }}</a>
+            <a href="{{ url('/home') }}">{{ Auth::user()->name }}</a>
           @endif
         </div>
       </div>
