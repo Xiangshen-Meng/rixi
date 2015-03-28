c719d35620392f86d681e3a3a8ab3bb0af1d7dc0
Xiangshen-Meng <wqnnjm@gmail.com>
2015-03-28 19:32:01 +0900

    Changed reset passwd page

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,39 +1,50 @@
 @extends('app')
 
 @section('content')
+  @include('shared.topbar')
 <div class="password-page">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-8 col-md-offset-2">
+      <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-success">
-          <div class="panel-heading">重置密码</div>
           <div class="panel-body">
-            @if (session('status'))
-              <div class="alert alert-success">
-                {{ session('status') }}
-              </div>
-            @endif
-
-            @include('auth.error_message')
-
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-              <div class="form-group">
-                <label class="col-md-4 control-label">邮箱</label>
-                <div class="col-md-6">
-                  <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+            <div class="rixi-panel-title">
+              重置密码
+            </div>
+            <div class="col-md-12">
+              @if (session('status'))
+                <div class="alert alert-success">
+                  {{ session('status') }}
                 </div>
-              </div>
+              @endif
 
-              <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                  <button type="submit" class="btn btn-primary">
-                    发送重置链接到邮箱
-                  </button>
+              @include('auth.error_message')
+
+              <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group">
+                  <label class="col-sm-2 col-lg-2 col-lg-offset-2 control-label">邮箱</label>
+                  <div class="col-sm-6 col-lg-4">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                  </div>
+                  <div class="col-sm-4 hidden-xs">
+                    <button type="submit" class="btn btn-primary">
+                      发送验证到邮箱
+                    </button>
+                  </div>
+                  <div class="col-xs-12 hidden-sm hidden-md hidden-lg">
+                    <button type="submit" class="btn btn-primary btn-block" style="margin-top: 10px">
+                      发送验证到邮箱
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
+
+            <div class="col-md-12">
+              <img src="/images/mail.svg" height="300" class="center-block">
+            </div>
           </div>
         </div>
       </div>
