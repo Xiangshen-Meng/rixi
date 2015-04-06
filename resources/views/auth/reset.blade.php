18d6d48faa92adef935bdd95b6d3af9fe925ce1e
Xiangshen-Meng <wqnnjm@gmail.com>
2015-04-06 18:45:51 +0900

    Added reset password

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,59 +1,62 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-<div class="row">
-<div class="col-md-8 col-md-offset-2">
-<div class="panel panel-default">
-<div class="panel-heading">Reset Password</div>
-<div class="panel-body">
-@if (count($errors) > 0)
-<div class="alert alert-danger">
-<strong>Whoops!</strong> There were some problems with your input.<br><br>
-<ul>
-@foreach ($errors->all() as $error)
-<li>{{ $error }}</li>
-@endforeach
-</ul>
-</div>
-@endif
+    @include('shared.topbar')
+    <div class="login-page">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            <div class="rixi-panel-title">
+                                重置密码
+                            </div>
+                            <div class="col-md-6">
+                                <img src="/images/hello.svg" height="300" class="center-block">
+                            </div>
+                            <div class="col-md-6">
+                                @include('auth.error_message')
 
-<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-<input type="hidden" name="_token" value="{{ csrf_token() }}">
-<input type="hidden" name="token" value="{{ $token }}">
+                                {!! Form::open(['url' => '/password/reset', 'class' => 'form-horizontal']) !!}
+                                    {!! Form::hidden('token', $token) !!}
+                                    <div class="form-group">
+                                        {!! Form::label('email', '邮箱', ['class' => 'col-md-3 control-label']) !!}
 
-<div class="form-group">
-<label class="col-md-4 control-label">E-Mail Address</label>
-<div class="col-md-6">
-<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-</div>
-</div>
+                                        <div class="col-md-9">
+                                            {!! Form::email('email', old('email'), ['class' => 'form-control
+                                            max-input-length']) !!}
+                                        </div>
+                                    </div>
 
-<div class="form-group">
-<label class="col-md-4 control-label">Password</label>
-<div class="col-md-6">
-<input type="password" class="form-control" name="password">
-</div>
-</div>
+                                    <div class="form-group">
+                                        {!! Form::label('password', '密码', ['class' => 'col-md-3 control-label']) !!}
 
-<div class="form-group">
-<label class="col-md-4 control-label">Confirm Password</label>
-<div class="col-md-6">
-<input type="password" class="form-control" name="password_confirmation">
-</div>
-</div>
+                                        <div class="col-md-9">
+                                            {!! Form::password('password', ['class' => 'form-control max-input-length']) !!}
+                                        </div>
+                                    </div>
 
-<div class="form-group">
-<div class="col-md-6 col-md-offset-4">
-<button type="submit" class="btn btn-primary">
-Reset Password
-</button>
-</div>
-</div>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
+                                    <div class="form-group">
+                                        {!! Form::label('password', '确认密码', ['class' => 'col-md-3 control-label']) !!}
+
+                                        <div class="col-md-9">
+                                            {!! Form::password('password_confirmation', ['class' => 'form-control
+                                            max-input-length']) !!}
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="col-md-9 col-md-offset-3">
+                                            <button type="submit" class="btn btn-primary">重置密码</button>
+                                        </div>
+                                    </div>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
