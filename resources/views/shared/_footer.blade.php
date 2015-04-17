84028032acd29054fc6cb77ad50ac1d6c86b8296
Xiangshen-Meng <wqnnjm@gmail.com>
2015-04-17 18:52:15 +0900

    Added traveljp to footer

--- resources/views/shared/_footer.blade.php
+++ resources/views/shared/_footer.blade.php
@@ -1,19 +1,20 @@
 <div class="bottom-menu">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-2 col-sm-2">
-        <a href="/" class="bottom-menu-brand">日系问答</a>
-      </div>
-      <div class="col-md-8 col-sm-8">
-        <ul class="bottom-menu-list">
-          <li><a href="http://shalery.net/" target="_blank">关于我们</a></li>
-          <li><a href="http://rixi.me/" target="_blank">日系广播站</a></li>
-        </ul>
-      </div>
-      <div class="col-md-2 col-sm-2">
-        <ul class="bottom-menu-iconic-list">
-        </ul>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-sm-2">
+                <a href="/" class="bottom-menu-brand">日系问答</a>
+            </div>
+            <div class="col-md-8 col-sm-8">
+                <ul class="bottom-menu-list">
+                    <li><a href="http://shalery.net/" target="_blank">关于我们</a></li>
+                    <li><a href="http://rixi.me/" target="_blank">日系广播站</a></li>
+                    <li><a href="http://transjp.info/" target="_blank">日本头条资讯</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-2">
+                <ul class="bottom-menu-iconic-list">
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
 </div> <!-- /bottom-menu -->
