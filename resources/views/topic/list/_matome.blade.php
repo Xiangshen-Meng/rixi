edb93eabd17d7d422872b2e7b160692fd53914aa
Xiangshen-Meng <wqnnjm@gmail.com>
2015-04-28 17:54:54 +0900

    Added matome description

--- resources/views/topic/list/_matome.blade.php
+++ resources/views/topic/list/_matome.blade.php
@@ -7,6 +7,9 @@
                     {{ $matome->title }}
                 </a>
             </h3>
+            <p>
+                {{ str_limit($matome->noFormatContent(), 20) }}
+            </p>
         </div>
     @empty
         <p>赶快来写第一个整理帖吧！</p>
